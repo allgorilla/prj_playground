e562a29b9f9b917574711606ce546cb034488c44
akinobu <akinobu.nakamura@eizo.com>
2021-03-08 18:08:09 +0900

    #1 リファクタ：一部のグローバル変数をPrivateメンバに変更

--- ex01/dungeon.py
+++ ex01/dungeon.py
@@ -16,15 +16,11 @@ DOTCOL_START_POS = (0,0,255,255)
 DOTCOL_FLOOR = (255,255,255,255)
 DOTCOL_WALL = (0,0,0,255)
 
-g_screen = None
-g_pygame = None
-
 g_img_black = None
 g_img_floor = None
-g_img_wall = None
-g_img_map = None
+g_img_wall  = None
+g_img_map   = None
 g_img_human = None
-g_move = None
 
 #-------------------------------------------------------------------------------
 # スタート位置を取得する
@@ -43,30 +39,27 @@ def get_start_pos(img):
 #-------------------------------------------------------------------------------
 # 画像表示（ブロック指定）
 #-------------------------------------------------------------------------------
-def put_img( img, x, y ):
-    global g_screen
-    global g_move
+def put_block( screen, move, img, x, y ):
 
-    pos_x = ( x * CELL_W ) - ( CELL_W/2 )
-    pos_y = ( y * CELL_H ) - ( CELL_H/2 )
-    move_x, move_y = g_move.get_move_offset()
-    g_screen.blit( img, ( pos_x + move_x, pos_y + move_y ))
+    pos_x = ( x * CELL_W ) - ( CELL_W / 2 )
+    pos_y = ( y * CELL_H ) - ( CELL_H / 2 )
+    move_x, move_y = move.get_move_offset()
+    screen.blit( img, ( pos_x + move_x, pos_y + move_y ))
     return
 #-------------------------------------------------------------------------------
 # プレイヤー表示（ブロック指定）
 #-------------------------------------------------------------------------------
-def put_player( img, x, y ):
-    global g_screen
+def put_player( screen, img, x, y ):
 
     pos_x = ( x * CELL_W ) - ( PLAYER_W/2 )
     pos_y = ( y * CELL_H ) - ( CELL_H/2 ) - ( PLAYER_H - CELL_H )
-    g_screen.blit( img, ( pos_x, pos_y ))
+    screen.blit( img, ( pos_x, pos_y ))
     return
 
 #-------------------------------------------------------------------------------
 # 床と壁のループ表示
 #-------------------------------------------------------------------------------
-def put_floor_and_wall( center_x, center_y ):
+def put_floor_and_wall( screen, move, center_x, center_y ):
     global g_img_map
     global g_img_floor
     global g_img_wall
@@ -91,7 +84,7 @@ def put_floor_and_wall( center_x, center_y ):
                         img = g_img_floor
                     elif dotcol == DOTCOL_WALL:
                         img = g_img_wall
-            put_img( img, ofs_x + sx, ofs_y + sy )
+            put_block( screen, move, img, ofs_x + sx, ofs_y + sy )
 
     return
 
@@ -100,29 +93,31 @@ def put_floor_and_wall( center_x, center_y ):
 #-------------------------------------------------------------------------------
 class SceneDungeon:
 
-    __cur_x = 0
-    __cur_y = 0
-    
+    #-------------------------------------------------------------------------------
+    # メンバ（Private）
+    #-------------------------------------------------------------------------------
+    __pygame = None
+    __scr    = None
+    __mv     = None
+    __px     = None
+    __py     = None
 
     #-------------------------------------------------------------------------------
     # コンストラクタ
     #-------------------------------------------------------------------------------
     def __init__( self, pygame, screen ):
-        global g_pygame
-        global g_screen
-        global g_move
         global g_img_black
         global g_img_floor
         global g_img_wall
         global g_img_map
         global g_img_human
 
-        g_pygame = pygame
-        g_screen = screen
+        self.__pygame = pygame
+        self.__scr    = screen
 
         # マップ読み込み
-        g_img_map = g_pygame.image.load("map.bmp")
-        find,self.__cur_x,self.__cur_y = get_start_pos(g_img_map)
+        g_img_map = self.__pygame.image.load("map.bmp")
+        find,self.__px,self.__py = get_start_pos(g_img_map)
 
         if find == False:
             print("")
@@ -131,37 +126,36 @@ class SceneDungeon:
             return
 
         # イメージの読み込み
-        g_img_black = g_pygame.image.load("black_64.bmp")
-        g_img_floor = g_pygame.image.load("floor.bmp")
-        g_img_wall = g_pygame.image.load("wall.bmp")
+        g_img_black = self.__pygame.image.load("black_64.bmp")
+        g_img_floor = self.__pygame.image.load("floor.bmp")
+        g_img_wall  = self.__pygame.image.load("wall.bmp")
 
         # プレイヤー読み込み
-        g_img_human = g_pygame.image.load("human_80.bmp").convert()
+        g_img_human = self.__pygame.image.load("human_80.bmp").convert()
         colorkey = g_img_human.get_at((0,0))
         g_img_human.set_colorkey(colorkey, RLEACCEL)
 
-        g_move = movemgr.MoveMgr()
-        g_move.set_destination( 10 )
-        g_move.set_block_size( CELL_H, CELL_W )
+        self.__mv = movemgr.MoveMgr()
+        self.__mv.set_destination( 10 )
+        self.__mv.set_block_size( CELL_H, CELL_W )
         return
     #-------------------------------------------------------------------------------
     # 描画
     #-------------------------------------------------------------------------------
     def draw( self ):
-        global g_screen
         global g_img_human
 
         # 画面を塗りつぶす
-        g_screen.fill(( 128, 128, 128 ))
+        self.__scr.fill(( 128, 128, 128 ))
 
         # 床と壁の表示
-        put_floor_and_wall( self.__cur_x,self.__cur_y )
+        put_floor_and_wall( self.__scr, self.__mv, self.__px,self.__py )
 
         #プレイヤーの表示
-        put_player( g_img_human, SCREEN_X/2, SCREEN_Y/2 )
+        put_player( self.__scr, g_img_human, SCREEN_X / 2, SCREEN_Y / 2 )
 
         # 描画処理を実行
-        pygame.display.update()
+        self.__pygame.display.update()
 
         return
 
@@ -170,34 +164,34 @@ class SceneDungeon:
     #-------------------------------------------------------------------------------
     def input( self ):
         # 移動オフセット進捗
-        if g_move.get_direction() != ( 0, 0 ):
-            g_pygame.time.wait( 16 );
-            g_move.make_progress()
+        if self.__mv.get_direction() != ( 0, 0 ):
+            self.__pygame.time.wait( 16 );
+            self.__mv.make_progress()
         else:
-            for event in g_pygame.event.get():
+            for event in self.__pygame.event.get():
 
                 # 終了イベント
                 if event.type == QUIT:  
                     #pygameのウィンドウを閉じる
-                    g_pygame.quit() 
+                    self.__pygame.quit() 
                     #システム終了
                     sys.exit()
 
                 # キ－入力イベント
                 elif event.type == KEYDOWN:
                     if event.key == K_ESCAPE:
-                        g_pygame.quit()
+                        self.__pygame.quit()
                         sys.exit()
                     elif event.key == K_UP:
-                        self.__cur_y -= 1
-                        g_move.set_direction( "up" )
+                        self.__py -= 1
+                        self.__mv.set_direction( "up" )
                     elif event.key == K_DOWN:
-                        self.__cur_y += 1
-                        g_move.set_direction( "down" )
+                        self.__py += 1
+                        self.__mv.set_direction( "down" )
                     elif event.key == K_LEFT:
-                        self.__cur_x -= 1
-                        g_move.set_direction( "left" )
+                        self.__px -= 1
+                        self.__mv.set_direction( "left" )
                     elif event.key == K_RIGHT:
-                        self.__cur_x += 1
-                        g_move.set_direction( "right" )
+                        self.__px += 1
+                        self.__mv.set_direction( "right" )
         return
