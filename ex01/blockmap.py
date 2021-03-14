279473d73ee0d2cdf4585fc10aa2e81079c2372b
allgorilla <pow.9093wgtm@docomo.ne.jp>
2021-03-14 22:11:00 +0900

    #7 リファクタ：床と壁の表示を別モジュールに分離

---
+++ ex01/blockmap.py
@@ -0,0 +1,157 @@
+# coding: utf-8
+from pygame.locals import *
+import pygame
+
+DOTCOL_START_POS = (0,0,255,255)
+DOTCOL_FLOOR = (255,255,255,255)
+DOTCOL_WALL = (0,0,0,255)
+
+g_img_map   = None
+g_img_black = None
+g_img_floor = None
+g_img_wall  = None
+
+#-------------------------------------------------------------------------------
+# ブロックマップ
+#-------------------------------------------------------------------------------
+class BlockMap:
+
+    __pygame  = None
+    __scr     = None
+    __map_w   = None
+    __map_h   = None
+    __block_w = None
+    __block_h = None
+
+    #-------------------------------------------------------------------------------
+    # コンストラクタ
+    # param(in) pygameオブジェクト
+    # param(in) screenオブジェクト
+    # param(in) マップのサイズ(X方向ブロック数)
+    # param(in) マップのサイズ(Y方向ブロック数)
+    # param(in) ブロックのサイズ(X方向ドット数)
+    # param(in) ブロックのサイズ(Y方向ドット数)
+    #-------------------------------------------------------------------------------
+    def __init__( self, pygame, screen, filename, map_w, map_h, block_w, block_h ):
+
+        global g_img_map
+        global g_img_black
+        global g_img_floor
+        global g_img_wall
+
+        self.__pygame  = pygame
+        self.__scr     = screen
+        self.__map_w   = map_w
+        self.__map_h   = map_h
+        self.__block_w = block_w
+        self.__block_h = block_h
+
+        # マップ読み込み
+        g_img_map = self.__pygame.image.load( filename )
+
+        # イメージの読み込み
+        g_img_black = self.__pygame.image.load( "black_64.bmp" )
+        g_img_floor = self.__pygame.image.load( "floor.bmp" )
+        g_img_wall  = self.__pygame.image.load( "wall.bmp" )
+
+        return
+
+    #-------------------------------------------------------------------------------
+    # スタート位置を取得する
+    #-------------------------------------------------------------------------------
+    def get_start_pos( self ):
+
+        rect = g_img_map.get_rect()
+        for y in range( rect.top, rect.bottom ):
+            for x in range( rect.left, rect.right ):
+                color = g_img_map.get_at(( x, y ))
+                if color == DOTCOL_START_POS:
+                    return True, x, y
+
+        return False, 0, 0
+
+    #-------------------------------------------------------------------------------
+    # 画像表示（ブロック指定）
+    #-------------------------------------------------------------------------------
+    def __put_block( self, move, img, x, y ):
+
+        pos_x = ( x * self.__block_w ) - ( self.__block_w / 2 )
+        pos_y = ( y * self.__block_h ) - ( self.__block_h / 2 )
+        move_x, move_y = move.get_move_offset()
+        self.__scr.blit( img, ( pos_x + move_x, pos_y + move_y ))
+
+        return
+
+    #-------------------------------------------------------------------------------
+    # ブロックマップの表示
+    # param(in) screenオブジェクト
+    # param(in) 移動中の方向
+    # param(in) プレイヤーのマップ上の現在地(X方向)
+    # param(in) プレイヤーのマップ上の現在地(Y方向)
+    #-------------------------------------------------------------------------------
+    def draw( self, move, cx, cy ):
+
+        rect = g_img_map.get_rect()
+        bx = int( self.__map_w / 2 ) # マップ→ウインドウの表示範囲の補正値(X方向)
+        by = int( self.__map_h / 2 ) # マップ→ウインドウの表示範囲の補正値(Y方向)
+        sx = bx                      # マップの表示範囲の開始ブロック(X方向)
+        sy = by                      # マップの表示範囲の開始ブロック(Y方向)
+        ex = bx + 1                  # マップの表示範囲の終端ブロック(X方向)
+        ey = by + 1                  # マップの表示範囲の終端ブロック(Y方向)
+
+        dir = move.get_direction()
+        if dir[ 0 ] == 1:
+            sx += 1
+        elif dir[ 0 ] == -1:
+            ex += 1
+
+        if dir[ 1 ] == 1:
+            sy += 1
+        elif dir[ 1 ] == -1:
+            ey += 1
+
+        for oy in range( -sy, ey ):
+            for ox in range( -sx, ex ):
+
+                mx = ox + cx # マップから読みだす対象のX座標
+                my = oy + cy # マップから読みだす対象のY座標
+
+                img = g_img_black
+                if mx >= rect.left and mx < rect.right:
+                    if my >= rect.top and my < rect.bottom:
+                        dotcol = g_img_map.get_at(( mx, my ))
+                        if dotcol == DOTCOL_START_POS:
+                            img = g_img_floor
+                        elif dotcol == DOTCOL_FLOOR:
+                            img = g_img_floor
+                        elif dotcol == DOTCOL_WALL:
+                            img = g_img_wall
+                self.__put_block( move, img, ox + bx, oy + by )
+
+        return
+
+    #-------------------------------------------------------------------------------
+    # マップイメージの範囲チェック
+    #-------------------------------------------------------------------------------
+    def __is_out_of_map( self, x, y ):
+
+        rect = g_img_map.get_rect()
+        if rect.top <= x and x < rect.bottom:
+            if rect.left <= y and y < rect.right:
+                return False
+
+        return True
+
+    #-------------------------------------------------------------------------------
+    # ブロックの侵入可否チェック
+    #-------------------------------------------------------------------------------
+    def can_walk( self, x, y ):
+
+        # マップイメージの範囲外に出る場合侵入不可
+        if True == self.__is_out_of_map( x, y ):
+            return False
+
+        elif DOTCOL_WALL != g_img_map.get_at(( x, y )):
+            return True
+
+        return False
--- ex01/dungeon.py
+++ ex01/dungeon.py
@@ -4,6 +4,8 @@ import pygame
 import sys
 import time
 import threading
+
+import blockmap
 import movemgr
 import keyevt
 
@@ -15,52 +17,8 @@ CELL_W = 64
 PLAYER_H = 80
 PLAYER_W = 80
 
-DOTCOL_START_POS = (0,0,255,255)
-DOTCOL_FLOOR = (255,255,255,255)
-DOTCOL_WALL = (0,0,0,255)
-
-g_img_black = None
-g_img_floor = None
-g_img_wall  = None
-g_img_map   = None
 g_img_human = None
 
-#-------------------------------------------------------------------------------
-# スタート位置を取得する
-#-------------------------------------------------------------------------------
-def get_start_pos(img):
-
-    rect = img.get_rect()
-    for y in range(rect.top,rect.bottom):
-        for x in range(rect.left,rect.right):
-            color = img.get_at((x,y))
-            if color == DOTCOL_START_POS:
-                return True,x,y
-
-    return False,0,0
-
-#-------------------------------------------------------------------------------
-# マップイメージの範囲チェック
-#-------------------------------------------------------------------------------
-def is_out_of_map_image( x, y ):
-
-    rect = g_img_map.get_rect()
-    if rect.top <= x and x < rect.bottom:
-        if rect.left <= y and y < rect.right:
-            return False
-
-    return True
-
-#-------------------------------------------------------------------------------
-# 画像表示（ブロック指定）
-#-------------------------------------------------------------------------------
-def put_block( screen, move, img, x, y ):
-
-    pos_x = ( x * CELL_W ) - ( CELL_W / 2 )
-    pos_y = ( y * CELL_H ) - ( CELL_H / 2 )
-    move_x, move_y = move.get_move_offset()
-    screen.blit( img, ( pos_x + move_x, pos_y + move_y ))
-    return
 #-------------------------------------------------------------------------------
 # プレイヤー表示（ブロック指定）
 #-------------------------------------------------------------------------------
@@ -72,58 +30,7 @@ def put_player( screen, img, x, y ):
     return
 
 #-------------------------------------------------------------------------------
-# 床と壁のループ表示
-# param(in) screenオブジェクト
-# param(in) 移動中の方向
-# param(in) プレイヤーのマップ上の現在地(X方向)
-# param(in) プレイヤーのマップ上の現在地(Y方向)
-#-------------------------------------------------------------------------------
-def put_floor_and_wall( screen, move, cx, cy ):
-    global g_img_map
-    global g_img_floor
-    global g_img_wall
-
-    rect = g_img_map.get_rect()
-    bx = int( SCREEN_X / 2 ) # マップ→ウインドウの表示範囲の補正値(X方向)
-    by = int( SCREEN_Y / 2 ) # マップ→ウインドウの表示範囲の補正値(Y方向)
-    sx = bx                  # マップの表示範囲の開始ブロック(X方向)
-    sy = by                  # マップの表示範囲の開始ブロック(Y方向)
-    ex = bx + 1              # マップの表示範囲の終端ブロック(X方向)
-    ey = by + 1              # マップの表示範囲の終端ブロック(Y方向)
-
-    dir = move.get_direction()
-    if dir[ 0 ] == 1:
-        sx += 1
-    elif dir[ 0 ] == -1:
-        ex += 1
-
-    if dir[ 1 ] == 1:
-        sy += 1
-    elif dir[ 1 ] == -1:
-        ey += 1
-
-    for oy in range( -sy, ey ):
-        for ox in range( -sx, ex ):
-
-            mx = ox + cx # マップから読みだす対象のX座標
-            my = oy + cy # マップから読みだす対象のY座標
-
-            img = g_img_black
-            if mx >= rect.left and mx < rect.right:
-                if my >= rect.top and my < rect.bottom:
-                    dotcol = g_img_map.get_at(( mx, my ))
-                    if dotcol == DOTCOL_START_POS:
-                        img = g_img_floor
-                    elif dotcol == DOTCOL_FLOOR:
-                        img = g_img_floor
-                    elif dotcol == DOTCOL_WALL:
-                        img = g_img_wall
-            put_block( screen, move, img, ox + bx, oy + by )
-
-    return
-
-#-------------------------------------------------------------------------------
-# 移動マネージャークラス
+# シーンクラス
 #-------------------------------------------------------------------------------
 class SceneDungeon:
 
@@ -135,6 +42,7 @@ class SceneDungeon:
     __mv     = None
     __px     = None
     __py     = None
+    __map     = None
     __key    = None
     __thread = None
 
@@ -142,34 +50,26 @@ class SceneDungeon:
     # コンストラクタ
     #-------------------------------------------------------------------------------
     def __init__( self, pygame, screen ):
-        global g_img_black
-        global g_img_floor
-        global g_img_wall
-        global g_img_map
         global g_img_human
 
         self.__pygame = pygame
         self.__scr    = screen
 
-        # マップ読み込み
-        g_img_map = self.__pygame.image.load("map.bmp")
-        find,self.__px,self.__py = get_start_pos(g_img_map)
+        # プレイヤー読み込み
+        g_img_human = self.__pygame.image.load("human_80.bmp").convert()
+        colorkey = g_img_human.get_at((0,0))
+        g_img_human.set_colorkey(colorkey, RLEACCEL)
+
+        # マップ初期化
+        self.__map = blockmap.BlockMap( self.__pygame, self.__scr, "map.bmp", SCREEN_X, SCREEN_Y, CELL_H, CELL_W )
+
+        find, self.__px, self.__py = self.__map.get_start_pos()
 
         if find == False:
             print("")
             print("【異常終了】マップにスタート地点が見つかりません")
             print("")
             return
-
-        # イメージの読み込み
-        g_img_black = self.__pygame.image.load("black_64.bmp")
-        g_img_floor = self.__pygame.image.load("floor.bmp")
-        g_img_wall  = self.__pygame.image.load("wall.bmp")
-
-        # プレイヤー読み込み
-        g_img_human = self.__pygame.image.load("human_80.bmp").convert()
-        colorkey = g_img_human.get_at((0,0))
-        g_img_human.set_colorkey(colorkey, RLEACCEL)
 
         self.__mv = movemgr.MoveMgr()
         self.__mv.set_destination( 12 )
@@ -208,20 +108,15 @@ class SceneDungeon:
         while None != self.__thread:
             self.__pygame.time.wait( 16 )
 
-            # 移動アニメーション
+            # 移動オフセットの進捗更新
             if self.__mv.get_direction() != ( 0, 0 ):
-                # 移動オフセット進捗
                 self.__mv.make_progress()
 
             else:
                 x, y = self.__key.get_direction()
 
-                # マップイメージの範囲外に出る場合侵入不可
-                if True == is_out_of_map_image( self.__px + x, self.__py + y ):
-                    pass
-
-                # ブロックが床の場合のみ侵入可
-                elif DOTCOL_WALL != g_img_map.get_at(( self.__px + x, self.__py + y )):
+                # ブロックの侵入可否チェック
+                if True == self.__map.can_walk( self.__px + x, self.__py + y ):
                     self.__px += x 
                     self.__py += y
                     self.__mv.set_direction( x, y )
@@ -236,7 +131,7 @@ class SceneDungeon:
         self.__scr.fill(( 0, 0, 255 ))
 
         # 床と壁の表示
-        put_floor_and_wall( self.__scr, self.__mv, self.__px,self.__py )
+        self.__map.draw( self.__mv, self.__px, self.__py )
 
         #プレイヤーの表示
         put_player( self.__scr, g_img_human, SCREEN_X / 2, SCREEN_Y / 2 )
