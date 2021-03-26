32661935db3655b8104486831380e8e8add260a7
allgorilla <pow.9093wgtm@docomo.ne.jp>
2021-03-26 18:28:37 +0900

    #10 仮コミット2 改良中

--- ex01/srf_fade_btl.py
+++ ex01/srf_fade_btl.py
@@ -28,6 +28,7 @@ class SrfFadeBattle:
     __list       = None
     __pos_list   = []
     __dir_list   = []
+    __dest       = None
 
     #-------------------------------------------------------------------------------
     # コンストラクタ
@@ -58,6 +59,7 @@ class SrfFadeBattle:
         self.__block_w  = block_w
         self.__block_h  = block_h
         self.__status   = status
+        self.__dest     = not initial
         self.__pos_list.append(( 0,0 ))
         self.__pos_list.append(( screen_w - 1,screen_h - 1 ))
         self.__pos_list.append(( 0,screen_h - 1 ))
@@ -73,26 +75,27 @@ class SrfFadeBattle:
     # アニメーションを進める
     #-------------------------------------------------------------------------------
     def make_progress( self ):
-        remove_list = []
         cnt = len( self.__pos_list )
-        for i in range( cnt ):
-            pos = self.__pos_list[ i ]
-            dir = self.__dir_list[ i ]
-            self.__list[ pos[1] ][ pos[0] ] = True
+        for i in range( cnt, 0, -1 ):
+            index = i - 1
+            pos = self.__pos_list[ index ]
+            dir = self.__dir_list[ index ]
+            self.__list[ pos[ 1 ] ][ pos[ 0 ] ] = self.__dest
 
             ret, dir = self.__is_finished( pos, dir )
             if True == ret:
-                remove_list.append( i )
+                self.__pos_list.pop( index )
+                self.__dir_list.pop( index )
             else:
-                self.__dir_list[ i ] = dir
-                self.__pos_list[ i ] = ( pos[0]+dir[0], pos[1]+dir[1] )
-
-        for i in remove_list:
-            self.__pos_list.pop( 0 )
-            self.__dir_list.pop( 0 )
+                self.__dir_list[ index ] = dir
+                self.__pos_list[ index ] = ( pos[ 0 ]  +dir[ 0 ], pos[ 1 ] + dir[ 1 ] )
 
     #-------------------------------------------------------------------------------
     # 侵入可能な方向があるか
+    #
+    # 進行方向を変えながら侵入できるブロックを探す
+    # 4方向（上下左右）をチェックして、
+    # いずれも侵入不可であれば探索を終了
     #-------------------------------------------------------------------------------
     def __is_finished( self, pos, dir ):
         if True == self.__can_move( pos, dir ):
@@ -114,11 +117,14 @@ class SrfFadeBattle:
 
     #-------------------------------------------------------------------------------
     # 侵入チェック
+    #
+    # 現在値に進行方向を加えた座標がマップの範囲外、
+    # または目標値に変更済みのブロックであればFalseをリターン
     #-------------------------------------------------------------------------------
     def __can_move( self, pos, dir ):
 
-        x = pos[0] + dir[0]
-        y = pos[1] + dir[1]
+        x = pos[ 0 ] + dir[ 0 ]
+        y = pos[ 1 ] + dir[ 1 ]
 
         if x < 0 or self.__screen_w <= x:
             return False
@@ -126,26 +132,32 @@ class SrfFadeBattle:
         if y < 0 or self.__screen_h <= y:
             return False
 
-        if self.__list[ y ][ x ] == True:
+        if self.__list[ y ][ x ] == self.__dest:
             return False
 
         return True
 
     #-------------------------------------------------------------------------------
-    # 侵入チェック
+    # 方向転換
+    #
+    # dirで指定された進行方向を90度回転する（反計回り）
+    # (1) 右 → 上
+    # (2) 上 → 左
+    # (3) 左 → 下
+    # (4) 下 → 右
     #-------------------------------------------------------------------------------
     def __change_dir( self, dir ):
 
-        if dir[0] == 1 and dir[1] == 0:
+        if dir[ 0 ] == 1 and dir[ 1 ] == 0:
             x = 0
             y = -1
-        elif dir[0] == 0 and dir[1] == -1:
+        elif dir[ 0 ] == 0 and dir[ 1 ] == -1:
             x = -1
             y = 0
-        elif dir[0] == -1 and dir[1] == 0:
+        elif dir[ 0 ] == -1 and dir[ 1 ] == 0:
             x = 0
             y = 1
-        elif dir[0] == 0 and dir[1] == 1:
+        elif dir[ 0 ] == 0 and dir[ 1 ] == 1:
             x = 1
             y = 0
 
@@ -158,7 +170,7 @@ class SrfFadeBattle:
         for y in range( self.__screen_h ):
             for x in range( self.__screen_w ):
                 if True == self.__list[ y ][ x ]:
-                    self.__screen.blit( self.__image, ( x * 64, y * 64 ))
+                    self.__screen.blit( self.__image, ( x * self.__block_w, y * self.__block_h ))
 
 
 
