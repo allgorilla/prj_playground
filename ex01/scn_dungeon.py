6df6c2f5d9c7be37c12b94cc3c487f6b5ccbe4c2
allgorilla <pow.9093wgtm@docomo.ne.jp>
2021-05-05 23:21:51 +0900

    #16 ポータルオブジェクトをマップに配置した

--- ex01/scn_dungeon.py
+++ ex01/scn_dungeon.py
@@ -11,6 +11,7 @@ import obj_party_player
 import obj_party_follower
 import obj_enemy_mino
 import obj_enemy_mummy
+import obj_portal_base
 import srf_wipe_btl
 import sts_cursor
 
@@ -46,15 +47,21 @@ class SceneDungeon( scn_base.SceneBase ):
         self.__map = srf_map.SrfMap( self.__pygame, "image/map1.bmp", screen_wh, cell_wh )
 
         # オブジェクト初期化
-        self.add_object( "image/human", cell_wh, 20, 100 )
-        self.add_object( "image/warrior", cell_wh, 20, 100 )
-        self.add_object( "image/thief", cell_wh, 20, 100 )
-        self.add_object( "image/mage", cell_wh, 20, 100 )
+        self.add_party_object( "image/human", cell_wh, 20, 100 )
+        self.add_party_object( "image/warrior", cell_wh, 20, 100 )
+        self.add_party_object( "image/thief", cell_wh, 20, 100 )
+        self.add_party_object( "image/mage", cell_wh, 20, 100 )
 
         for i in range( 6 ):
-            self.add_object( "image/enemy_mino", cell_wh, 10, 20 )
-            self.add_object( "image/enemy_mummy", cell_wh, 40, 100 )
-            self.add_object( "image/enemy_dragon", cell_wh, 20, 50 )
+            self.add_enemy_object( "image/enemy_mino", cell_wh, 10, 20 )
+            self.add_enemy_object( "image/enemy_mummy", cell_wh, 40, 100 )
+            self.add_enemy_object( "image/enemy_dragon", cell_wh, 20, 50 )
+
+        self.add_portal_object( "image/stairs_d", cell_wh, 20, 100 )
+        self.add_portal_object( "image/circle", cell_wh, 20, 100 )
+        self.add_portal_object( "image/circle", cell_wh, 20, 100 )
+        self.add_portal_object( "image/circle", cell_wh, 20, 100 )
+        self.add_portal_object( "image/circle", cell_wh, 20, 100 )
 
         # 状態管理 - カーソル
         self.__cursor = sts_cursor.StsCursor()
@@ -66,20 +73,36 @@ class SceneDungeon( scn_base.SceneBase ):
     #-------------------------------------------------------------------------------
     # 敵を追加するサブルーチン
     #-------------------------------------------------------------------------------
-    def add_object( self, file, cell_wh, acnt, tcnt ):
-
+    def add_party_object( self, file, cell_wh, acnt, tcnt ):
         if 0 == len( self.__obj_list ):
-            pos = self.__map.get_portal_pos()
+            pos = self.__map.get_start_pos()
             object = obj_party_player.ObjectPartyPlayer( self.__pygame, pos, cell_wh, acnt, tcnt )
         elif 0 < len( self.__obj_list ) and len( self.__obj_list ) < 4:
             pos = self.__obj_list[ 0 ].loc_pos
             object = obj_party_follower.ObjectPartyFollower( self.__pygame, pos, cell_wh, acnt, tcnt )
-        else:
-            pos = self.__map.get_enemy_pos()
-            object = obj_enemy_mummy.ObjectEnemyMummy( self.__pygame, pos, cell_wh, acnt, tcnt )
 
         object.add_pattern( file + "_a.png" )
         object.add_pattern( file + "_b.png" )
+        self.__obj_list.append( object )
+
+    #-------------------------------------------------------------------------------
+    # 敵を追加するサブルーチン
+    #-------------------------------------------------------------------------------
+    def add_enemy_object( self, file, cell_wh, acnt, tcnt ):
+        pos = self.__map.get_enemy_pos()
+        object = obj_enemy_mummy.ObjectEnemyMummy( self.__pygame, pos, cell_wh, acnt, tcnt )
+
+        object.add_pattern( file + "_a.png" )
+        object.add_pattern( file + "_b.png" )
+        self.__obj_list.append( object )
+
+    #-------------------------------------------------------------------------------
+    # 敵を追加するサブルーチン
+    #-------------------------------------------------------------------------------
+    def add_portal_object( self, file, cell_wh, acnt, tcnt ):
+        pos = self.__map.get_portal_pos()
+        object = obj_portal_base.ObjectPortalBase( self.__pygame, pos, cell_wh, acnt, tcnt )
+        object.add_pattern( file + ".png" )
         self.__obj_list.append( object )
 
     #-------------------------------------------------------------------------------
@@ -150,17 +173,12 @@ class SceneDungeon( scn_base.SceneBase ):
 
         # 全オブジェクトの優先度リストを作成
         pri_list = []
+        i = 0;
         for object in self.__obj_list:
             # Y座標は大きいほど優先
             y = object.blit_pos[ 1 ]
-            abs_x = abs( self.__obj_list[ 0 ].blit_pos[ 0 ] - object.blit_pos[ 0 ] )
-            # X座標はプレイヤーが移動中か停止中かで優先を変更する
-            if self.__obj_list[ 0 ].move.is_stop():
-                # 中心に遠いほど優先
-                x = abs_x
-            else:
-                # 中心に近いほど優先
-                x = 1000 - abs_x
+            x = len( self.__obj_list ) - i
+            i += 1
             pri_list.append( y * 1000 + x )
 
         # 優先度リストから表示順リストを作成
